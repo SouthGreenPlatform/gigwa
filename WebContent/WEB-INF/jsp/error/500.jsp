591ae723888fd3b7b0260cc52f5b96c1f5b21d2a
sempere <guilhem.sempere@cirad.fr>
2018-03-06 12:54:02 +0100

    Support for project removal.

--- WebContent/WEB-INF/jsp/error/500.jsp
+++ WebContent/WEB-INF/jsp/error/500.jsp
@@ -20,8 +20,8 @@
 
 <html>
     <head>
-		<link rel ="stylesheet" type="text/css" href="<c:url value="/css/main.css" />" title="style">
-		<script type="text/javascript" src="<c:url value="/js/jquery-1.8.2.min.js" />"></script>
+		<link rel ="stylesheet" type="text/css" href="css/role_manager.css" title="style">
+		<script type="text/javascript" src="<c:url value="/private/js/jquery-1.12.4.min.js" />"></script>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Error 500</title>
     </head>
--- src/fr/cirad/tools/data/GigwaModuleManager.java
+++ src/fr/cirad/tools/data/GigwaModuleManager.java
@@ -1,21 +1,40 @@
 package fr.cirad.tools.data;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import fr.cirad.mgdb.model.mongo.maintypes.GenotypingProject;
+import fr.cirad.mgdb.model.mongo.maintypes.Individual;
+import fr.cirad.mgdb.model.mongo.maintypes.VariantRunData;
+import fr.cirad.mgdb.model.mongo.maintypes.VariantRunData.VariantRunDataId;
+import fr.cirad.mgdb.model.mongodao.MgdbDao;
 import fr.cirad.security.base.IModuleManager;
 import fr.cirad.tools.mongo.MongoTemplateManager;
+import fr.cirad.tools.security.TokenManager;
+import fr.cirad.tools.security.base.AbstractTokenManager;
 
 @Component
 public class GigwaModuleManager implements IModuleManager {
 
+	private static final Logger LOG = Logger.getLogger(GigwaModuleManager.class);
+	
+	@Autowired TokenManager tokenManager;
+	
+	
 	@Override
 	public Collection<String> getModules(Boolean fTrueForPublicFalseForPrivateNullForBoth) {
 		if (fTrueForPublicFalseForPrivateNullForBoth == null)
@@ -77,5 +96,70 @@ public class GigwaModuleManager implements IModuleManager {
 	@Override
 	public Collection<String> getHosts() {
 		return MongoTemplateManager.getHostNames();
+	}
+	
+	@Override
+	public boolean removeManagedEntity(String sModule, String sEntityType, Comparable entityId) throws Exception {
+		if (AbstractTokenManager.ENTITY_PROJECT.equals(sEntityType))
+		{
+			final int nProjectIdToRemove = Integer.parseInt(entityId.toString());
+			if (!tokenManager.canUserWriteToProject(SecurityContextHolder.getContext().getAuthentication(), sModule, nProjectIdToRemove))
+				throw new Exception("You are not allowed to remove this project");
+
+			MongoTemplate mongoTemplate = MongoTemplateManager.get(sModule);
+			Query query = new Query();
+			query.fields().include(GenotypingProject.FIELDNAME_SAMPLES);
+			List<String> individualsInThisProject = null, individualsInOtherProjects = new ArrayList<>();
+			int nProjCount = 0;
+			for (GenotypingProject proj : mongoTemplate.find(query, GenotypingProject.class))
+			{
+				nProjCount++;
+				List<String> projectIndividuals = proj.getSamples().values().stream().map(sp -> sp.getIndividual()).collect(Collectors.toList());
+				if (proj.getId() == nProjectIdToRemove)
+					individualsInThisProject = projectIndividuals;
+				else
+					individualsInOtherProjects.addAll(projectIndividuals);
+			}
+			if (nProjCount == 1 && !individualsInThisProject.isEmpty())
+			{
+				mongoTemplate.getDb().dropDatabase();
+				LOG.debug("Dropped database for module " + sModule + " instead of removing its only project");
+				return true;
+			}
+
+			Collection<String> individualsToRemove = CollectionUtils.disjunction(individualsInThisProject, CollectionUtils.intersection(individualsInThisProject, individualsInOtherProjects));
+			int nRemovedIndCount = mongoTemplate.remove(new Query(Criteria.where("_id").in(individualsToRemove)), Individual.class).getN();
+			LOG.debug("Removed " + nRemovedIndCount + " individuals out of " + individualsInThisProject.size());
+
+			if (mongoTemplate.remove(new Query(Criteria.where("_id").is(nProjectIdToRemove)), GenotypingProject.class).getN() > 0)
+				LOG.debug("Removed project " + nProjectIdToRemove + " from module " + sModule);
+			
+			new Thread() {
+				public void run() {
+					int nRemovedVrdCount = mongoTemplate.remove(new Query(Criteria.where("_id." + VariantRunDataId.FIELDNAME_PROJECT_ID).is(nProjectIdToRemove)), VariantRunData.class).getN();
+					LOG.debug("Removed " + nRemovedVrdCount + " VRD records for project " + nProjectIdToRemove + " of module " + sModule);
+				}
+			}.start();
+			LOG.debug("Launched async VRD cleanup for project " + nProjectIdToRemove + " of module " + sModule);
+			
+            mongoTemplate.getCollection(MgdbDao.COLLECTION_NAME_CACHED_COUNTS).drop();
+			return true;
+		}
+		else
+			throw new Exception("Not managing entities of type " + sEntityType);
+	}
+
+	@Override
+	public boolean doesEntityExistInModule(String sModule, String sEntityType, Comparable entityId) {
+		if (AbstractTokenManager.ENTITY_PROJECT.equals(sEntityType))
+		{
+			final int nProjectId = Integer.parseInt(entityId.toString());
+			return MongoTemplateManager.get(sModule).count(new Query(Criteria.where("_id").is(nProjectId)), GenotypingProject.class) == 1;
+		}
+		else
+		{
+			LOG.error("Not managing entities of type " + sEntityType);
+			return false;
+		}
 	}
 }
